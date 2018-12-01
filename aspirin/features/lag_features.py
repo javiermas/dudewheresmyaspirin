c25e226db28f478d4554520e14894e1e8a134d78
feliuserra <feliuserraburriel@gmail.com>
2018-12-01 18:43:09 +0100

    Remove Sales 1.

--- aspirin/features/lag_features.py
+++ aspirin/features/lag_features.py
@@ -5,7 +5,7 @@ def lag_multiseries(multiseries, lags=list(range(1, 12))):
     '''
     all_lagged = DataFrame(index=multiseries.index)
     unlagged_cols = ['Investment 1', 'Investment 2', 'Investment 3', 'Investment 4',
-       'Investment 5', 'Investment 6', 'Sales 1', 'Sales 2']
+       'Investment 5', 'Investment 6', 'Sales 2']
 
     for lag in lags:
         lagged_series = multiseries.groupby(['Cluster', 'Brand Group'])[unlagged_cols].shift(lag)
