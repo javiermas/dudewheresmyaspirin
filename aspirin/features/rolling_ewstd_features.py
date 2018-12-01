5a49d06d07b8ea9fdf0c5149a952716dfc70203e
feliuserra <feliuserraburriel@gmail.com>
2018-12-01 17:54:13 +0100

    :bug::notebook: Fix feature creation and add notebook on first ensembles.

--- aspirin/features/rolling_ewstd_features.py
+++ aspirin/features/rolling_ewstd_features.py
@@ -5,8 +5,7 @@ def rolling_ewstd_multiseries(multiseries, lags=[6, 12, 18, 24]):
     '''
 
     all_rolled = DataFrame(index=multiseries.index)
-    unrolled_cols = ['Investment 1', 'Investment 2', 'Investment 3', 'Investment 4',
-        'Investment 5', 'Investment 6', 'Sales 1', 'Sales 2']
+    unrolled_cols = [col for col in multiseries.columns if 'lag' in col]
     rolling_windows = lags
 
     for window in rolling_windows:
